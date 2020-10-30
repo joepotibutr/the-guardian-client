60cdb6c43085017baea484f0b0461e347ce02e2c
Fjoepotibutr <vchrpng@gmail.com>
2020-10-31 01:44:14 +0700

    update article detail styles

--- src/pages/ArticleDetail/index.tsx
+++ src/pages/ArticleDetail/index.tsx
@@ -1,20 +1,60 @@
-import React, { Component } from 'react'
+import React, { ReactElement } from 'react'
+import axios from 'axios'
+import { useLocation } from 'react-router-dom'
+import { ArticleDetailStyled } from './styled'
 
 interface Props {
     
 }
-interface State {
-    
+
+export interface Content {
+    apiUrl: string
+    id: string
+    isHosted: boolean
+    pillarId: string
+    pillarName: string
+    sectionId:  string
+    sectionName: string
+    type: string
+    webPublicationDate: Date
+    webTitle: string
+    webUrl: string
 }
 
-export default class ArticleDetail extends Component<Props, State> {
-    state = {}
+export default function ArticleDetail({}: Props): ReactElement {
+    const [currentArticle, setCurrentArticle] = React.useState<null | Content>(null)
+    const [fetchError, setErrors] = React.useState(null)
 
-    render() {
-        return (
-            <div>
-                ArticleDetail
-            </div>
-        )
+    let location = useLocation();
+
+  React.useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:8888/article',{
+            params: {
+                id: location.pathname
+            }
+        })
+        setCurrentArticle(data.response.content)
+      } catch (err) {
+        setErrors(err)
+      }
     }
+    fetchArticles()
+  }, [])
+
+    return (
+        <ArticleDetailStyled>
+            {!fetchError && currentArticle ? (
+                <div className="article">
+                    <div className="publish-detail">
+                        <span>{currentArticle.sectionName} |</span>
+                        <span> {currentArticle.type} |</span>
+                        <span> {new Date(currentArticle.webPublicationDate).toLocaleDateString()}</span>
+                    </div>
+                    <h1 className="web-title" onClick={() => window.open(currentArticle.webUrl)}>{currentArticle.webTitle}</h1>
+                </div>
+            ) : <h1>Loading ...</h1>}
+        </ArticleDetailStyled>
+    )
 }
---
+++ src/pages/ArticleDetail/styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const ArticleDetailStyled = styled.div`
+    margin-top: 50px;
+
+    .article {
+        text-align: left;
+
+        .publish-detail {
+            cursor: default;
+        }
+    }
+
+    .web-title {
+        color: #0E2A5E;
+        cursor: pointer;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+
+`
