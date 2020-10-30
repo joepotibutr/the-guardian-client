3c941bb5478b330a3fa4524e289d4958cc136b7b
Fjoepotibutr <vchrpng@gmail.com>
2020-10-31 01:43:27 +0700

    update page routes

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -4,25 +4,41 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  useRouteMatch
 } from "react-router-dom";
+import axios from 'axios'
 
+import Header from '../../components/Header'
 import ArticleList from '../ArticleList'
-import ArticleDetail from '../ArticleDetail';
+import ArticleDetail, { Content } from '../ArticleDetail';
 
 function App() {
+  const [articles, setArticles] = React.useState<Array<Content>>([])
+  const [fetchError, setErrors] = React.useState(null)
+
+  React.useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:8888')
+        setArticles(data.response.results || [])
+      } catch (err) {
+        setErrors(err)
+      }
+    }
+    fetchArticles()
+  }, [])
 
   return (
     <Router>
+      <Header setArticles={(articles: Array<Content>) => setArticles(articles)}/>
       <AppStyledWrapper>
-      <Switch>
+        <Switch>
             <Route path={`/:articleId`}>
               <ArticleDetail />
             </Route>
             <Route path="/">
-              <ArticleList />
+              <ArticleList articles={articles}/>
             </Route>
-          </Switch>
+        </Switch>
       </AppStyledWrapper>
     </Router>
     
--- src/pages/App/styled.ts
+++ src/pages/App/styled.ts
@@ -3,19 +3,9 @@ import styled from 'styled-components'
 
 export const AppStyledWrapper = styled.div`
   text-align: center;
-
-
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
-
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 20px;
 
 
 `
